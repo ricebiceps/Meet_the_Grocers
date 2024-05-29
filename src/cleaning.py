8f0eeb4e3d161dc7c7697fd2c897cd619722c6b6
ricebiceps <ricebiceps@gmail.com>
2024-05-29 17:55:36 -0400

    some changes to the data transformation file to enable csv load onto bigquery

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-path = r'/home/Park/workspace/github.com/ricebiceps/meet_the_grocers/'
+path = r'/home/Park/workspace/github.com/ricebiceps/meet_the_hoteliers/'
 
 # Read the CSV file into a Polars DataFrame with specific encoding and error handling
 try:
@@ -10,11 +10,22 @@ try:
         infer_schema_length=10000,
         ignore_errors=True
     )
-    # Display the first 5 rows
+    
+    # Drop the "Description" column
+    df = df.drop(" Description")
+    
+    # Filter the DataFrame
+    df_filtered = df.filter(pl.col(" countyName") == "Canada")
+    
+    # Add row index if the DataFrame is not empty
+    if df_filtered.height > 0:
+        df_filtered = df_filtered.with_row_count("row_index")
+    
+    # Write the filtered DataFrame to a new CSV file
+    df_filtered.write_csv(f'{path}/data/hotel_data_canada.csv', separator=",")
+    
+    # Display the first 5 rows of the filtered DataFrame
+    print(df_filtered.head(5))
+    
 except Exception as e:
     print(f"An error occurred: {e}")
-
-df = df.filter(pl.col(" countyName") == "Canada")
-row_count = df.with_row_index()
-
-df.write_csv(f'{path}/data/hotel_data_canada.csv', separator=",")
